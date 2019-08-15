04fe1f15c5e618bb895a6e97009fdddf81e3cb52
Andrei K <1____1@bk.ru>
2019-08-15 16:55:47 +0300

    add time streaming to archive page

--- server.py
+++ server.py
@@ -1,9 +1,24 @@
 from aiohttp import web
 import aiofiles
+import datetime
+import asyncio 
 
+INTERVAL_SECS = 1
 
 async def archivate(request):
-    raise NotImplementedError
+    response = web.StreamResponse()
+    
+    response.headers['Content-Type']  = 'text/html'
+    await response.prepare(request)
+    
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS) 
 
 
 async def handle_index_page(request):
