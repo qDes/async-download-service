80d133f075650fe9577411d9f6cfbd2e8faa4969
Andrei K <1____1@bk.ru>
2019-08-22 16:21:02 +0300

    add command line arguments handler

--- server.py
+++ server.py
@@ -6,43 +6,44 @@ import datetime
 import asyncio 
 import os
 import logging 
+import argparse
+import functools
 
-#logging.basicConfig(level = logging.DEBUG)
-logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG)#, filename = u'mylog.log')
-
-async def archivate(request):
+async def archivate(request,photo_folder,delay):
     '''Async function archives photos and send it to client by chunks '''
+    
 
     #get photo folder path
-    folder = 'test_photos/'+request.path.split('/')[2] 
+    folder = photo_folder + request.match_info['archive_hash']
     #check folder path
     if not os.path.exists(folder):
-        raise HTTPNotFound(text='Archive does not exist')
+        raise HTTPNotFound(text='Archive does not exist or was deleted.')
     #create respose
     response = web.StreamResponse()
     response.headers['Content-Type'] = 'multipart/form-data'
     response.headers['Content-Disposition'] = 'attachment; filename = "archive.zip"'
     #create archivate subprocess
     cmd = ['zip','-r','-',folder]
-    create = asyncio.create_subprocess_exec(*cmd,
+    create_process = asyncio.create_subprocess_exec(*cmd,
             stdout = asyncio.subprocess.PIPE)
-    proc = await create
+    archivate_proc = await create_process
     await response.prepare(request)
     try:    
         while True:
             # read bytes from archive
-            archive_chunk = await proc.stdout.read(1024)
+            archive_chunk = await archivate_proc.stdout.read(32768)
             if archive_chunk:
                 #send archive chunk to client
                 await response.write(archive_chunk)
                 logging.debug( u'Sending archive chunk ...')
-                #await asyncio.sleep(1)
+                if delay:
+                    await asyncio.sleep(int(delay))
             else:
                 await response.write_eof()
                 return response
     except asyncio.CancelledError:
         #terminate archive process on connection interruption
-        proc.terminate()
+        archivate_proc.terminate()
         raise
     finally:
         response.force_close()
@@ -54,10 +55,35 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-if __name__ == '__main__':
+def main():
+    '''Run web service '''
+
+    
+    #read command line arguments
+    delay, folder, logger = parse_args()
+    if logger:
+        logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG)    
+
     app = web.Application()
+    partial_archivate = functools.partial(archivate,photo_folder = folder, delay = delay)
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
+        web.get('/archive/{archive_hash}/',partial_archivate),
     ])
-    web.run_app(app)
+    web.run_app(app)   
+    
+
+def parse_args():
+    '''Parse command line argumetns'''
+
+    parser = argparse.ArgumentParser(description='Async download service')
+    parser.add_argument('-log', action = 'store_true',help='on/off debug messages')
+    parser.add_argument('indir', type=str, help ='photos directory') 
+    parser.add_argument('-delay',action='store',help='set delay as value in seconds')
+    args = parser.parse_args()
+    
+    return args.delay, args.indir, args.log
+
+
+if __name__ == '__main__':
+    main()
